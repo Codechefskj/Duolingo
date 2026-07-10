19c1e75fc788becf344b817bb29b286c3039fec4
SAMBHAV JHA <sambhavjha57@gmail.com>
2026-07-11 04:40:04 +0530

    Fix Vercel configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -12,7 +12,8 @@ app = FastAPI(title="Duolingo Clone API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000",
+        "https://duolingo-26bg.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
